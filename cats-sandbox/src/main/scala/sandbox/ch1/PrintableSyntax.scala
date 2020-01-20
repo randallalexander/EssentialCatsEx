74699a08e2877010a8ae246f220d21c7ec536a4e
Randall Alexander <Randall.Alexander@gmail.com>
2020-01-20 11:05:32 -0800

    Exercise: Printable Library - Beetter Syntax

---
+++ cats-sandbox/src/main/scala/sandbox/ch1/PrintableSyntax.scala
@@ -0,0 +1,12 @@
+package sandbox.ch1
+
+object PrintableSyntax {
+  implicit class PrintableOps[A] (value: A) {
+    def format (implicit printable: Printable[A]): String = {
+      printable.format(value)
+    }
+    def print (implicit printable: Printable[A]): Unit = {
+      println(format)
+    }
+  }
+}
